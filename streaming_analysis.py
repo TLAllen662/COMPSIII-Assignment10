12fec347e88e4c887503c79ae435eafdd476b5b4
TLAllen662 <tlallen1419@gmail.com>
2025-11-05 12:31:01 -0600

    Add calculation for top 5 most listened to artists, songs, and genres

--- streaming_analysis.py
+++ streaming_analysis.py
@@ -14,4 +14,11 @@ total_time = df['listening_time'].sum()
 total_minutes = total_time / 60
 total_hours = total_time / 3600
 
+# Get top 5 most listened to artists
+top_artists = df['artist'].value_counts().head(5)
 
+# Get top 5 most listened to songs
+top_songs = df['song_title'].value_counts().head(5)
+
+# Get top 5 most listened to genres
+top_genres = df['genre'].value_counts().head(5)
